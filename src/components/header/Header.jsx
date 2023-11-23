c57542d5c6c7f81c5beaa09469a4fea3fc83d459
odilo <odilomurindahabi@gmail.com>
2023-11-23 22:06:17 +0200

    add CTA component into Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/odilo.jpg'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Odilo Murindahabi</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#"></a>
+      </div>
+    </header>
   )
 }
 
