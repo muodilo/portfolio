b937776ff2e78f9ab1f236aec98032c85b4763a6
odilo <odilomurindahabi@gmail.com>
2023-11-24 00:03:40 +0200

    add icons to nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { IoHomeOutline } from "react-icons/io5";
+import { FaRegUser } from "react-icons/fa";
+import { BiBook } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
+import { MdOutlineMessage } from "react-icons/md";
 import './nav.css'
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><IoHomeOutline/></a>
+      <a href="#about"><FaRegUser/></a>
+      <a href="#experience"><BiBook/></a>
+      <a href="#services"><RiServiceLine/></a>
+      <a href="#contact"><MdOutlineMessage/></a>
+    </nav>
   )
 }
 
